f17ed878f5699264498871cfc4d475e40957f126
unknown <roya.abouhi@gmail.com>
2022-05-31 09:55:47 +0300

    hw

---
+++ day16-hw/js/main.js
@@ -0,0 +1,20 @@
+const $ = {
+email:document.querySelector(#email),
+password:document.querySelector(#password),
+email:document.querySelector(button),
+}
+
+const DB = {
+
+}
+var user = {
+
+}
+
+var l =["a","b","c","d","e","f","g","h","i",
+"j","k","l",,"m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+function makeToken(){
+var a = Math.floor(Math.random() * 10);
+var b = l[Math.floor(math.random()l.length*10)]
+}
+
